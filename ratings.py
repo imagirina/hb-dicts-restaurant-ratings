0e5cc6edb54936c6791c99e54cc050ce4b343d4d
agira <imagirina@gmail.com>
2022-04-04 18:50:30 -0700

    Added functions

--- ratings.py
+++ ratings.py
@@ -1,21 +1,42 @@
 """Restaurant rating lister."""
 
-def read_ratings(file_name):
+def process_data(file_name):
+    """Read data file line by line and return dictionary of {restaurant_name : score}"""
+
     content = open(file_name)
-    restaurant_information = {}
-    # ratings = []
+    restaurant_info = {}
+    
     for line in content:
         line = line.rstrip()
         line = line.split(":")        
-        restaurant_information[line[0]] = int(line[1]) 
-           
-    content.close()      
+        restaurant_info[line[0]] = int(line[1])
+        # restaurant, score = line.split(":")
+        # scores[restaurant] = int(score)
+    content.close()
 
-    added_restaurant = input('What restaurant would you like to rate? ')
-    added_rating = input('How would you rate that restaurant from 1 to 5? ')
-    restaurant_information[added_restaurant] = int(added_rating)
+    return restaurant_info
+
+
+def add_restaurant(restaurant_info):
+    """Add a restaurant and rating."""
     
-    for name, score in sorted(restaurant_information.items()):
-        print(f"{name} is rated at {score}.")
+    restaurant = input('What restaurant would you like to rate? ')
+    score = input('How would you rate that restaurant from 1 to 5? ')
+    restaurant_info[restaurant] = int(score)
+    
 
-read_ratings('scores.txt')
+def print_sorted_scores(scores):
+    """Print sorted restaurant names and scores."""
+
+    for restaurant, score in sorted(restaurant_info.items()):
+        print(f"{restaurant} is rated at {score}.")
+
+
+# Proceesing data from file
+restaurant_info = process_data('scores.txt')
+
+# Prompt user for new restaurant name and rating
+add_restaurant(restaurant_info)
+
+# Print in alphabetical order the list or restaurants and ratings
+print_sorted_scores(restaurant_info)
